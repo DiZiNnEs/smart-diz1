088aaec1f255e56e188e72ad8996172afa446b6c
dizinnes <dizinnes228@gmail.com>
2022-04-23 19:07:37 +0600

    refactor: Update config files

--- src/services/nlp/nlp.ts
+++ src/services/nlp/nlp.ts
@@ -1,8 +1,12 @@
 import { dockStart } from '@nlpjs/basic'
 
 export const getAnswer = async (text: string): Promise<string> => {
-    const dock = await dockStart({ use: ['Basic'] })
+    // const confJsFileDir = __dirname.split('/src')[0] + 'conf.js'
+    const confJsFileDir = __dirname + '/conf.json'
+    const dock = await dockStart({ settings: confJsFileDir })
     const nlp = dock.get('nlp')
-    const response = await nlp.process('en', text)
+    await nlp.train()
+
+    const response = await nlp.process('ru', text)
     return response.answer
 }
