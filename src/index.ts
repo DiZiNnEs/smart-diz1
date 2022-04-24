618d66db245bd721ab95de23eef1a05f6233fe42
dizinnes <dizinnes228@gmail.com>
2022-04-24 12:41:34 +0600

    feat(api): Create training api

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 
-import { getAnswer } from './services/nlp/nlp'
+import { getAnswer, startTraining } from './services/nlp/nlp'
 
 
 const app = express()
 app.use(bodyParser.json())
 const PORT = 3000
 
-app.get('/api/getAnswer/', async (req: express.Request, res: express.Response) => {
+app.get('/api/v1/getAnswer/', async (req: express.Request, res: express.Response) => {
     const question = req.body.question
     const answer = await getAnswer(question)
     return res.status(200).json({ message: answer })
+})
+
+app.get('/api/v1/training/', async (req: express.Request, res: express.Response) => {
+    try {
+        await startTraining()
+        return res.status(200).json({ message: 'success' })
+    } catch (err) {
+        return res.status(500).json({ message: err })
+    }
 })
 
 app.listen(PORT, () => {
--- src/services/nlp/nlp.ts
+++ src/services/nlp/nlp.ts
@@ -3,10 +3,15 @@ import { dockStart } from '@nlpjs/basic'
 export const getAnswer = async (text: string): Promise<string> => {
     // const confJsFileDir = __dirname.split('/src')[0] + 'conf.js'
     const confJsFileDir = __dirname + '/conf.json'
-    const dock = await dockStart({ settings: confJsFileDir })
+    const dock = await dockStart()
     const nlp = dock.get('nlp')
-    await nlp.train()
 
     const response = await nlp.process('ru', text)
     return response.answer
+}
+
+export const startTraining = async (): Promise<void> => {
+    const dock = await dockStart()
+    const nlp = dock.get('nlp')
+    await nlp.train()
 }
