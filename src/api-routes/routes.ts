4f0804a9d727672a60b43a3a1ce07fdb8c4f2275
dizinnes <dizinnes228@gmail.com>
2022-05-01 11:13:56 +0600

    refactor(api/routes): Move router in separate file

---
+++ src/api-routes/routes.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import { getAnswer, startTraining } from '../services/nlp/nlp'
+
+export const router = express.Router()
+
+router.get('/api/v1/getAnswer/', async (req: express.Request, res: express.Response) => {
+    const question = req.body.question
+    const answer = await getAnswer(question)
+    return res.status(200).json({ message: answer })
+})
+
+router.get('/api/v1/training/', async (req: express.Request, res: express.Response) => {
+    try {
+        await startTraining()
+        return res.status(200).json({ message: 'success' })
+    } catch (err) {
+        return res.status(500).json({ message: err })
+    }
+})
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,14 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 
-import { getAnswer, startTraining } from './services/nlp/nlp'
+import { router } from './api-routes/routes'
 
-
-const app = express()
-app.use(bodyParser.json())
 const PORT = 3000
+const app = express()
 
-app.get('/api/v1/getAnswer/', async (req: express.Request, res: express.Response) => {
-    const question = req.body.question
-    const answer = await getAnswer(question)
-    return res.status(200).json({ message: answer })
-})
+app.use(bodyParser.json())
 
-app.get('/api/v1/training/', async (req: express.Request, res: express.Response) => {
-    try {
-        await startTraining()
-        return res.status(200).json({ message: 'success' })
-    } catch (err) {
-        return res.status(500).json({ message: err })
-    }
-})
+app.use(router)
 
 app.listen(PORT, () => {
     console.log('Started')
