047620f060589247b4ced6fb64cfc08d89d3398a
dizinnes <dizinnes228@gmail.com>
2022-05-01 11:15:30 +0600

    refactor(services/npl): Delete training.ts

--- src/services/nlp/training.ts
+++
@@ -1,8 +0,0 @@
-import { dockStart } from '@nlpjs/basic'
-
-export const training = async () => {
-    const dock = await dockStart({ use: ['Basic'] })
-    const nlp = dock.get('nlp')
-    await nlp.addCorpus('./corpus-en.json')
-    await nlp.train()
-}
