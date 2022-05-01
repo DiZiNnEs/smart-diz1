d1f29b57b54dfe831ae9d81c0b68f30d1d4351ff
dizinnes <dizinnes228@gmail.com>
2022-05-01 14:28:05 +0600

    feat(bot/telegram): Create telegram bot

--- src/bot/telegram/telegramBot.ts
+++ src/bot/telegram/telegramBot.ts
@@ -1,21 +1,6 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import * as tg from 'telegraf'
+import Telegraf from 'telegraf'
 
-import { router } from './api-routes/routes'
-
-const PORT = 3000
-const app = express()
-
-app.use(bodyParser.json())
-
-app.use(router)
-
-app.listen(PORT, () => {
-    console.log('Started')
-})
-
-const bot = new tg.Telegraf('5316086155:AAEvkRpuLaLF-WDYRtmAdhyXOrMssJO_P8Y')
+const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx: { reply: (arg0: string) => any }) => ctx.reply('Welcome'))
 bot.help((ctx: { reply: (arg0: string) => any }) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx: { reply: (arg0: string) => any }) => ctx.reply('👍'))
