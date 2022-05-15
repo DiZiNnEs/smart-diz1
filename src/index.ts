5cf6164efd21f9a2548f8235694059bf3256a1b4
dizinnes <1233213dizi>
2022-05-15 12:01:33 +0600

    feat(services): Add training while project is start

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,27 @@
-import express from 'express'
-import bodyParser from 'body-parser'
+import express from "express";
+import bodyParser from "body-parser";
 
 
-import { router } from './api-routes/routes'
-import { telegramBot } from './bot/telegram/telegramBot'
+import { router } from "./api-routes/routes";
+import { telegramBot } from "./bot/telegram/telegramBot";
+import { startTraining } from "./services/nlp/nlp";
 
-const PORT = 3000
-const app = express()
+const PORT = 3000;
+const app = express();
 
-app.use(bodyParser.json())
 
-app.use(router)
+app.use(router);
 
-app.listen(PORT, () => {
-    console.log('Started')
-})
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
 
-telegramBot.launch()
+// parse application/json
+app.use(bodyParser.json());
+
+app.listen(PORT, async () => {
+  await startTraining();
+  console.log("Started");
+});
+
+telegramBot.launch();
 
