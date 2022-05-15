b727d4357442f7abfbe22393bef92765a7d482d9
dizinnes <1233213dizi>
2022-05-15 12:00:52 +0600

    feat(services): Minor update for telegram bot service

--- src/bot/telegram/telegramBot.ts
+++ src/bot/telegram/telegramBot.ts
@@ -1,10 +1,11 @@
-import * as tg from 'telegraf'
+import * as tg from "telegraf";
+import { getAnswer } from "../../services/nlp/nlp";
 
-export const telegramBot = new tg.Telegraf('5316086155:AAEvkRpuLaLF-WDYRtmAdhyXOrMssJO_P8Y')
-telegramBot.start((ctx: { reply: (arg0: string) => any }) => ctx.reply('Добро пожаловать, я бот'))
-telegramBot.help((ctx: { reply: (arg0: string) => any }) => ctx.reply('Просто поговори со мной'))
-telegramBot.on('text', async (ctx: any) => ctx.reply(ctx.update.message.text))
+export const telegramBot = new tg.Telegraf("5316086155:AAEvkRpuLaLF-WDYRtmAdhyXOrMssJO_P8Y");
+telegramBot.start((ctx: { reply: (arg0: string) => any }) => ctx.reply("Добро пожаловать, я бот"));
+telegramBot.help((ctx: { reply: (arg0: string) => any }) => ctx.reply("Просто поговори со мной"));
+telegramBot.on("text", async (ctx: any) => ctx.reply(await getAnswer(ctx.update.message.text)));
 
 // Enable graceful stop
-process.once('SIGINT', () => telegramBot.stop('SIGINT'))
-process.once('SIGTERM', () => telegramBot.stop('SIGTERM'))
+process.once("SIGINT", () => telegramBot.stop("SIGINT"));
+process.once("SIGTERM", () => telegramBot.stop("SIGTERM"));
