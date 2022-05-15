92f7e9baa272ddadd195aadf0c6389a092efcb4b
dizinnes <1233213dizi>
2022-05-15 12:00:41 +0600

    feat(services): Minor update for nlp service

--- src/services/nlp/nlp.ts
+++ src/services/nlp/nlp.ts
@@ -1,17 +1,17 @@
-import { dockStart } from '@nlpjs/basic'
+import { dockStart } from "@nlpjs/basic";
 
 export const getAnswer = async (text: string): Promise<string> => {
-    // const confJsFileDir = __dirname.split('/src')[0] + 'conf.js'
-    const confJsFileDir = __dirname + '/conf.json'
-    const dock = await dockStart()
-    const nlp = dock.get('nlp')
+  // const confJsFileDir = __dirname.split('/src')[0] + 'conf.js'
+  // const confJsFileDir = __dirname + '/conf.json'
+  const dock = await dockStart();
+  const nlp = dock.get("nlp");
 
-    const response = await nlp.process('ru', text)
-    return response.answer
-}
+  const response = await nlp.process("ru", text);
+  return response.answer ? response.answer : "Ух, затрудняюсь ответить)";
+};
 
 export const startTraining = async (): Promise<void> => {
-    const dock = await dockStart()
-    const nlp = dock.get('nlp')
-    await nlp.train()
-}
+  const dock = await dockStart();
+  const nlp = dock.get("nlp");
+  await nlp.train();
+};
